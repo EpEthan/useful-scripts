e430214299db9abb39c67cdb95d71768e26445f7
Eitan Epstein <epsteineitan@gmail.com>
2020-11-11 22:26:11 +0200

    Alter help, organize layout, change method name.

--- PS/gig/gig.ps1
+++ PS/gig/gig.ps1
@@ -1,7 +1,11 @@
 <#
 
-.Synopsis
+.SYNOPSIS
 gig - Download .gitignore files from www.toptal.com/developers/gitignore
+
+.DESCRIPTION
+Download .gitignore file templates based on the given arguments, to a desired path.
+If -List is passed, the list of available template options will be printed.
 
 .PARAMETER List
 Whether to print all the options for .gitignore files
@@ -11,27 +15,58 @@ Aliases: Lst, L
 The environments/langs you want in the .gitignore.
 For example: 'VisualStudio,Csharp,Go'
 
+.PARAMETER SavePath
+Where the .gitignore file should be saved.
+Default value: $pwd
+
+.INPUTS
+You can pipe a path to the gig.ps1, to choose the path of the 
+resulting .gitignore file.
+
+.OUTPUTS
+If you pass -List to gig.ps1, the program will output the list of 
+available .gitignore template options.
+
+.NOTES
+  Version:        1.0
+  Author:         Eitan Epstein
+  Creation Date:  11/11/2020
+
+
 .EXAMPLE
 PS> gig -l
 
 .EXAMPLE
 PS> gig VisualStudio,Csharp
 
+
+.LINK
+https://github.com/EpEthan/useful-scripts/tree/main/PS/gig
+More commands on GitHub: https://github.com/EpEthan/useful-scripts
+
 #> 
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
 # Parameters
 param(
-    # Parameter to list all available options
     [Alias("lst", "l")]
     [switch] $List,
-
+    
     [string[]] $ToIgnore,
-
-    [string] $SavePath=$pwd
+    
+    [Parameter(ValueFromPipeline = $true)]
+    [string] $SavePath = $pwd
 )
 
-Function GetIgnore 
-{
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+$sScriptVersion = "1.0"
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+Function Get-Ignore {
     param(
         [Parameter(Mandatory = $true)]
         [string[]]$lst,
@@ -55,10 +90,12 @@ Function GetIgnore
 
 
 
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
 
 if ($List) {
     Write-Output "fuck you. i ain't doing this shit."
 }
 else {
-    GetIgnore -lst $ToIgnore -path $SavePath
+    Get-Ignore -lst $ToIgnore -path $SavePath
 }
